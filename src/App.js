2e2c8f87217acfb8075c6b10195d5eed3fea560a
Sahil-238 <Sdhawale03@hotmail.com>
2025-05-29 17:41:26 +0530

    added blog and contact pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Introduction from './pages/Introduction';
 import About from './pages/About';
+import Blog from './pages/Blog';
+import BlogDetail from './pages/BlogDetail';
+import Contact from './pages/Contact';
 import WebDevelopment from './pages/WebDevelopment';
 import DataScience from './pages/DataScience';
 import MobileDevelopment from './pages/MobileDevelopment';
@@ -24,6 +27,9 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/introduction" element={<Introduction />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:id" element={<BlogDetail />} />
         <Route path="/courses/web-development" element={<WebDevelopment />} />
         <Route path="/courses/data-science" element={<DataScience />} />
         <Route path="/courses/mobile-development" element={<MobileDevelopment />} />
