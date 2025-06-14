d22c67c4b96f238d0d6ac55d402d29589ab1361b
Sahil-238 <sdhawale03@hotmail.com>
2025-06-14 21:04:24 +0530

    test 22

--- craco.config.js
+++ craco.config.js
@@ -1,43 +1,59 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = {
+//   webpack: {
+//     configure: (webpackConfig) => {
+//       // Add .jsx extension to the list of resolvable extensions
+//       webpackConfig.resolve.extensions = ['.js', '.jsx', '.json'];
+      
+//       // Update module resolution
+//       webpackConfig.resolve.modules = [
+//         path.resolve(__dirname, 'src'),
+//         'node_modules'
+//       ];
+
+//       // Add fallbacks for Node.js core modules
+//       webpackConfig.resolve.fallback = {
+//         "path": require.resolve("path-browserify"),
+//         "fs": false,
+//         "url": false,
+//         "util": false,
+//         "stream": false,
+//         "crypto": false
+//       };
+      
+//       // Update the module rules to handle .jsx files
+//       const babelLoader = webpackConfig.module.rules.find(
+//         (rule) => rule.loader && rule.loader.includes('babel-loader')
+//       );
+      
+//       if (babelLoader) {
+//         babelLoader.options = {
+//           ...babelLoader.options,
+//           plugins: [
+//             ...(babelLoader.options.plugins || []),
+//             '@babel/plugin-syntax-dynamic-import'
+//           ]
+//         };
+//       }
+      
+//       return webpackConfig;
+//     },
+//   },
+// }; 
+
 
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      // Add .jsx extension to the list of resolvable extensions
-      webpackConfig.resolve.extensions = ['.js', '.jsx', '.json'];
-      
-      // Update module resolution
-      webpackConfig.resolve.modules = [
-        path.resolve(__dirname, 'src'),
-        'node_modules'
-      ];
-
-      // Add fallbacks for Node.js core modules
-      webpackConfig.resolve.fallback = {
-        "path": require.resolve("path-browserify"),
-        "fs": false,
-        "url": false,
-        "util": false,
-        "stream": false,
-        "crypto": false
+      webpackConfig.devServer = {
+        ...webpackConfig.devServer,
+        setupMiddlewares: (middlewares, devServer) => {
+          if (!devServer) throw new Error('webpack-dev-server is not defined');
+          return middlewares;
+        }
       };
-      
-      // Update the module rules to handle .jsx files
-      const babelLoader = webpackConfig.module.rules.find(
-        (rule) => rule.loader && rule.loader.includes('babel-loader')
-      );
-      
-      if (babelLoader) {
-        babelLoader.options = {
-          ...babelLoader.options,
-          plugins: [
-            ...(babelLoader.options.plugins || []),
-            '@babel/plugin-syntax-dynamic-import'
-          ]
-        };
-      }
-      
       return webpackConfig;
-    },
-  },
-}; 
+    }
+  }
+};
